916940a50bd769b0fe34affda94ade2421939b55
gcoss <gcoss@cinepolis.com>
2025-03-30 14:28:58 -0600

    first commit

---
+++ app/(general)/contact/page.tsx
@@ -0,0 +1,14 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Contact",
+  description: "Contact page",
+};
+
+export default function ContactPage() {
+  return(
+    <>
+    <span className="text-7xl">ContactPage</span>
+    </>
+  )
+}
